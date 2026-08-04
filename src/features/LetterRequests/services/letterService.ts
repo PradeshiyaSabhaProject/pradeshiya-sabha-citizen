3cfeb1513f7041b11f0d034fedf7e38a1828dff3
Sachinthe Gamachchi <sachinthawg36@gmail.com>
2026-08-04 10:29:17 +0530

    900 error fix

--- src/features/LetterRequests/services/letterService.ts
+++ src/features/LetterRequests/services/letterService.ts
@@ -100,7 +100,7 @@ export const letterService = {
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
+    hours = hours || 12; // the hour '0' should be '12'
     const timeStr = `${String(hours).padStart(2, '0')}.${minutes} ${ampm}`;
 
     const request = {
