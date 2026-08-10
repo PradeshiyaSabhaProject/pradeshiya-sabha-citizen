0c2b0063f9684ac1797b7de61f246f22df3e286f
Thuhina Nimnadira <nimnadirathuhina@gmail.com>
2026-08-10 21:27:40 +0530

    GDP 945

--- src/components/ServiceTile/ServiceTile.tsx
+++ src/components/ServiceTile/ServiceTile.tsx
@@ -2,22 +2,11 @@ import React from 'react';
 
 const ServiceTile = ({ title, description, icon, onClick }) => {
   return (
-    <div
-      className="service-tile"
-      onClick={onClick}
-      onKeyDown={(e) => {
-        if ((e.key === 'Enter' || e.key === ' ') && onClick) {
-          e.preventDefault();
-          onClick(e);
-        }
-      }}
-      role="button"
-      tabIndex={0}
-    >
+    <button type="button" className="service-tile" onClick={onClick}>
       {icon && <div className="service-icon">{icon}</div>}
       <h4 className="service-title">{title}</h4>
       <p className="service-desc">{description}</p>
-    </div>
+    </button>
   );
 };
 
