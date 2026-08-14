1d080bfc47fffc42ecfb6be839a2be3819959f7e
Thuhina Nimnadira <nimnadirathuhina@gmail.com>
2026-08-15 01:37:52 +0530

    GDP 935-34

--- src/pages/Auth/RegisterView.tsx
+++ src/pages/Auth/RegisterView.tsx
@@ -57,10 +57,11 @@ const RegisterView = ({ onCancel }) => {
   };
 
   const handleFileChange = (e, side) => {
-    if (e.target.files && e.target.files[0]) {
+    const file = e.target.files?.[0];
+    if (file) {
       setFormData(prev => ({
         ...prev,
-        [side]: e.target.files[0].name
+        [side]: file.name
       }));
     }
   };
